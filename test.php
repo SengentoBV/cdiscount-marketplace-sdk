10a6756955acf398ba4094a39890147326981fcd
Tim Mylemans <tim@mylemans.com>
2020-09-10 10:28:01 +0200

    You can now specify a api user/pw in .env to perform the basic test
    Added extra api call

--- test.php
+++ test.php
@@ -6,9 +6,12 @@ use SengentoBV\CdiscountMarketplaceSdk\Structs\CdiscountOfferFilter;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $apiClient = new CdiscountMarketplaceApiClient();
 
-$apiClient->setFaultHandler(new CdiscountTokenRetrievingFaultHandler('', ''));
+$apiClient->setFaultHandler(new CdiscountTokenRetrievingFaultHandler($_ENV['CDISCOUNT_API_USERNAME'], $_ENV['CDISCOUNT_API_PASSWORD']));
 
 $filter = new \SengentoBV\CdiscountMarketplaceSdk\Structs\CdiscountOrderFilter();
 $filter->setBeginCreationDate('2020-07-01T00:00:00.00');
@@ -20,3 +23,8 @@ $filter->setFetchParcels(true);
 $result = $apiClient->order()->getOrderList($filter);
 
 print_r($result->getGetOrderListResult()->getOrderList());
+
+
+$response = $apiClient->order()->getGlobalConfiguration();
+
+print_r($response);
